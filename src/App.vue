afb18632054d6c73480daae8cdb67f4f08bb8682
Chris Frame <chris.frame@drivenworldwide.com>
2022-03-09 12:51:03 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<n-space>
+I'm trying to set the most restrictive <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP">CSP</a> possible, however as an example, the buttons below have inline styles that then require 'unsafe-inline' style-src.
+</n-space>
+   <n-space>
+    <n-button>Default</n-button>
+    <n-button type="tertiary">
+      Tertiary
+    </n-button>
+    <n-button type="primary">
+      Primary
+    </n-button>
+    <n-button type="info">
+      Info
+    </n-button>
+    <n-button type="success">
+      Success
+    </n-button>
+    <n-button type="warning">
+      Warning
+    </n-button>
+    <n-button type="error">
+      Error
+    </n-button>
+  </n-space>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { NSpace, NButton } from 'naive-ui';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    NSpace,
+    NButton
   }
 });
 </script>
